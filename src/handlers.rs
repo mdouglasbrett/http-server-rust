dd8074f62a453a61ce155e028247fd31be87dbd1
Michael Brett <mdouglasbrett@icloud.com>
2024-10-25 08:31:30 +0100

    wip: removes unused check and comment

--- src/handlers.rs
+++ src/handlers.rs
@@ -45,23 +45,15 @@ pub fn handle_get_file<T: Write>(
     fp: Arc<Mutex<Option<String>>>,
 ) -> Result<(), AppError> {
     let filename = get_path_parts(&r.path)[1];
-    // TODO: this _might_ break the api
-    // TODO: should we be doing the 404 off the back of this error?
-    // TODO: can we do this in the router? and get rid of the empty check?
     let contents = read_file(fp, filename)?;
     let encoding = get_header_value(headers::ACCEPT_ENCODING, &r.headers);
-    // TODO: is this legit now?
-    if contents.is_empty() {
-        handle_error(s, NotFound.into())
-    } else {
-        let response = Response::Ok(Some((
-            &contents,
-            mime_types::OCTET_STREAM.to_owned(),
-            encoding,
-        )));
-        s.write_all(&response.to_vec())?;
-        Ok(())
-    }
+    let response = Response::Ok(Some((
+        &contents,
+        mime_types::OCTET_STREAM.to_owned(),
+        encoding,
+    )));
+    s.write_all(&response.to_vec())?;
+    Ok(())
 }
 
 pub fn handle_post_file<T: Write>(
@@ -92,18 +84,18 @@ pub fn handle_error<T: Write>(s: &mut T, err: AppError) -> Result<(), AppError> 
 
 #[cfg(test)]
 mod test {
-    use std::io::Write;
-    use crate::http::response::Response;
     use crate::handlers::handle_empty as empty;
+    use crate::http::response::Response;
+    use std::io::Write;
 
     struct MockStream {
-        write_all_bytes: Vec<u8>
+        write_all_bytes: Vec<u8>,
     }
 
     impl MockStream {
         fn new() -> Self {
             Self {
-                write_all_bytes: vec!()
+                write_all_bytes: vec![],
             }
         }
     }
@@ -118,7 +110,6 @@ mod test {
             self.write_all_bytes.extend(buf);
             Ok(())
         }
-
     }
     #[test]
     fn handle_empty() {
