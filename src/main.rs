cd20bd5d0b6e69d7d72449ab3f2e22bce0cdb7a8
Michael Brett <mdouglasbrett@icloud.com>
2024-10-26 18:32:51 +0100

    wip: adds dep for light cli front end

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,27 @@ use server::app_server;
 
 pub type Result<T> = std::result::Result<T, AppError>;
 
+#[derive(Debug, Default)]
+struct Config {
+    dir: String,
+    port: String,
+}
+
+impl Config {
+    fn parse() -> Result<Self> {
+        use lexopt::prelude::*;
+        let mut config = Config::default();
+        Ok(config)
+    }
+}
+
 fn main() -> Result<()> {
-    // TODO: cli front end
     let mut args = env::args();
     let _ = args.next();
     // --directory flag
     let _ = args.next();
     let listener = TcpListener::bind("127.0.0.1:4221")?;
 
+    // TODO: pass a Config object
     app_server(args.next(), listener)
 }
