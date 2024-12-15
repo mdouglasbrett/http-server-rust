5db50a5097291e126035839784db7fafabc58eba
Michael Brett <mdouglasbrett@icloud.com>
2024-12-15 14:39:21 +0000

    wip: adds intial pass at graceful shutdown
    Uses ctrl-c crate for listener, lacks proper error handling

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use std::time::Duration;
 
 use crate::config::Config;
 use crate::router::request_router;
@@ -8,14 +10,25 @@ use crate::Result;
 
 pub fn app_server(config: Config) -> Result<()> {
     let listener = TcpListener::bind(&config.address)?;
+    listener.set_nonblocking(true)?;
     println!("Server listening on: {}", config.address);
     let partial_file_path = Arc::new(config.directory);
     let pool = ThreadPool::new(8);
 
-    for stream in listener.incoming() {
-        let path = Arc::clone(&partial_file_path);
-        match stream {
-            Ok(stream) => {
+    let running = Arc::new(AtomicBool::new(true));
+    let r = Arc::clone(&running);
+
+    ctrlc::set_handler(move || {
+        println!("Starting shutdown...");
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("TODO: handle ctrlc error");
+
+    while running.load(Ordering::SeqCst) {
+        match listener.accept() {
+            Ok((stream, addr)) => {
+                println!("Connection from: {}", addr);
+                let path = Arc::clone(&partial_file_path);
                 pool.execute(move || {
                     // TODO: error handling here, should I just eprintln! and continue?
                     if let Err(_e) = request_router(stream, path) {
@@ -25,11 +38,21 @@ pub fn app_server(config: Config) -> Result<()> {
                     }
                 });
             }
+            // The listener is non-blocking, so if there are no connections waiting we can just sleep for a bit and try again
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                std::thread::sleep(Duration::from_millis(100));
+                continue;
+            }
+            // If there is an error accepting a connection, we'll just print it and continue
             Err(e) => {
-                panic!("Panicked with: {:?}", e);
+                eprintln!("Connection error: {:?}", e);
+                continue;
             }
         }
     }
 
+    println!("Shutting down server...");
+    drop(pool);
+    println!("Server shutdown complete.");
     Ok(())
 }
