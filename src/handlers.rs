05902ffd32fbb589d58475c0725946cbde800298
Michael Brett <mdouglasbrett@icloud.com>
2025-06-02 20:03:18 +0100

    chore: moves handlers to where clauses
    This is a yak shave as I'm trying to figure out the file stuff...

--- src/handlers.rs
+++ src/handlers.rs
@@ -10,7 +10,10 @@ use std::{
 };
 
 #[derive(Debug)]
-pub struct HandlerArg<'a, T: Write> {
+pub struct HandlerArg<'a, T>
+where
+    T: Write,
+{
     pub req: &'a Request,
     pub stream: &'a mut T,
     // TODO: might get away with a &'a str here
@@ -31,7 +34,10 @@ pub trait Handler {
 }
 
 impl Handler for EchoHandler {
-    fn handle<T: Write>(r: HandlerArg<T>) -> Result<()> {
+    fn handle<T>(r: HandlerArg<T>) -> Result<()>
+    where
+        T: Write,
+    {
         let body = r.req.body.as_slice();
         let resp = Response::builder()
             .body(Some(body.to_owned()))
@@ -43,7 +49,10 @@ impl Handler for EchoHandler {
     }
 }
 impl Handler for EmptyHandler {
-    fn handle<T: Write>(r: HandlerArg<T>) -> Result<()> {
+    fn handle<T>(r: HandlerArg<T>) -> Result<()>
+    where
+        T: Write,
+    {
         let resp = Response::ok()?;
         r.stream.write_all(&resp.as_bytes())?;
         Ok(())
@@ -52,7 +61,10 @@ impl Handler for EmptyHandler {
 // TODO: this has side effects, how do we do this well?
 // TODO: use temp dir, manage it via cargo env?
 impl Handler for FileHandler {
-    fn handle<T: Write>(r: HandlerArg<T>) -> Result<()> {
+    fn handle<T>(r: HandlerArg<T>) -> Result<()>
+    where
+        T: Write,
+    {
         let filename = &r.req.path_parts[1];
         let path = Path::new(r.target_dir).join(filename);
         if r.req.method == Method::Get {
@@ -77,7 +89,10 @@ impl Handler for FileHandler {
 }
 
 impl Handler for UserAgentHandler {
-    fn handle<T: Write>(r: HandlerArg<T>) -> Result<()> {
+    fn handle<T>(r: HandlerArg<T>) -> Result<()>
+    where
+        T: Write,
+    {
         let b = r
             .req
             .get_header(Headers::UserAgent)
@@ -93,7 +108,10 @@ impl Handler for UserAgentHandler {
 }
 
 impl Handler for NotFoundHandler {
-    fn handle<T: Write>(r: HandlerArg<T>) -> Result<()> {
+    fn handle<T>(r: HandlerArg<T>) -> Result<()>
+    where
+        T: Write,
+    {
         let resp = Response::not_found()?;
         r.stream.write_all(&resp.as_bytes())?;
         Ok(())
@@ -101,7 +119,11 @@ impl Handler for NotFoundHandler {
 }
 
 impl ErrorHandler {
-    pub fn handle<T: Write>(a: (&mut T, AppError)) -> Result<()> {
+    // TODO: make this arg a concrete type?
+    pub fn handle<T>(a: (&mut T, AppError)) -> Result<()>
+    where
+        T: Write,
+    {
         match a.1 {
             AppError::Client(ClientError::BadRequest) => {
                 let resp = Response::client_error()?;
